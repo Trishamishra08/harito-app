8238a88d97478f6596d5ec3ab14237653aba17e3
Trishamishra08 <trishamishra115@gmail.com>
2026-04-21 18:02:26 +0530

    updatation in backend folder

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(cors({
       process.env.FRONTEND_URL,
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      'https://www.hirato.in',
       'https://hirato-app.vercel.app'
     ].filter(Boolean);
 
